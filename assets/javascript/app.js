2b55d109ef049ddc97ca21123f40d8bc2db58706
Hongsyn Keath <Hongsyn Keath>
2017-07-12 20:42:40 -0400

    Updated basic HTML layout, some css designs, and early stages of javascript layout.

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// Questions and answers object
+var allQuestions = [{
+	
+	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+	choices: {
+		correct: "Phillip",
+		incorrect: ["John", "Peter", "Keith"]
+	}
+},{
+	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+	choices: {
+		correct: "Phillip",
+		incorrect: ["John", "Peter", "Keith"]
+	}
+},{
+	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+	choices: {
+		correct: "Phillip",
+		incorrect: ["John", "Peter", "Keith"]
+	}
+},{
+	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+	choices: {
+		correct: "Phillip",
+		incorrect: ["John", "Peter", "Keith"]
+	}
+},{
+	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+	choices: {
+		correct: "Phillip",
+		incorrect: ["John", "Peter", "Keith"]
+	}
+}];
+// Functions to be called
+
+// Start game function
+
