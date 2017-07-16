0078147c632ea3d36da4f8a5f4d57d56d1283d3c
Hongsyn Keath <Hongsyn Keath>
2017-07-16 18:25:11 -0400

    Added timer countdown. Working on displaying questions and answer choices.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,16 @@
+window.onload = function() {
+    
+    $("#startButton").on("click", function() {
+        $("#startButton").hide();
+        $("#subContent").show();
+        run();
+    })
+};
+
+
 // Questions and answers object
 var allQuestions = [{
-	
-	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
-	choices: {
-		correct: "Phillip",
-		incorrect: ["John", "Peter", "Keith"]
-	}
-},{
+
 	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
 	choices: {
 		correct: "Phillip",
@@ -31,7 +35,55 @@ var allQuestions = [{
 		incorrect: ["John", "Peter", "Keith"]
 	}
 }];
+// --------------------------------------------------------------------
+// Global variables
+// ----------------
+var intervalId;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unanswered = 0;
+var number = 60;
+
+// --------------------------------------------------------------------
 // Functions to be called
+// ----------------------
 
-// Start game function
+// create for loop to generate divs to hold questions and answers
+for (i = 0; i < allQuestions.length; i++) {
+    $("#subContent").html("<div>" + allQuestions[i] + "</div>");
+}
 
+function run() {
+    intervalId = setInterval(decrement, 1000);
+}
+
+//  The decrement function.
+function decrement() {
+
+    //  Decrease number by one.
+    number--;
+
+    //  Show the number in the #show-number tag.
+    $("#show-number").html(number);
+
+
+    //  Once number hits zero...
+    if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+    }
+}
+
+function stop() {
+
+  console.log("stopping");
+  clearInterval(intervalId);
+
+}
+    // --------------------------------------------------------------------
+    // Start game function
+    // -------------------
