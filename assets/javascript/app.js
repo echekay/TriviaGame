bdbe4b34b93de6a43d4177699c24f597008e98c9
Hongsyn Keath <Hongsyn Keath>
2017-07-16 21:10:09 -0400

    Final rough draft to be submitted. Much of game logic is still missing after hitting roadblock.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,24 @@
+// When start button is clicked, have start button disappear; generate timer, questions, and answers.
+// Have timer set to decrement every second from a total of 60 seconds.
+// Have user have only one option to select an answer for each question.
+// Run if statements to see if they're answers are correct and add to either correct or incorrect counters.
+// If user picks an snwer for all questions before timer runs out, display results summary.
+// If time runs out before user finishes, display results summary.
+// Have option to start new game without having to refresh page.
+// ---------------------------------------------------------------
+// To-do: 
+// Figure out a way to iterate throup the object and create new divs and populate them with the questions in that object.
+// Also to create radio buttons with the answer choices through a for loop.
+// Need to create click events for each radio button to save as user answer and compare in if statement to correct answer.
+// Need to add counter with each correct or incorrect choice.
+// Need to define when game ends, display results and start button.
 window.onload = function() {
-    
+
     $("#startButton").on("click", function() {
         $("#startButton").hide();
         $("#subContent").show();
         run();
+        makesQuiz();
     })
 };
 
@@ -11,30 +26,29 @@ window.onload = function() {
 // Questions and answers object
 var allQuestions = [{
 
-	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
-	choices: {
-		correct: "Phillip",
-		incorrect: ["John", "Peter", "Keith"]
-	}
-},{
-	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
-	choices: {
-		correct: "Phillip",
-		incorrect: ["John", "Peter", "Keith"]
-	}
-},{
-	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
-	choices: {
-		correct: "Phillip",
-		incorrect: ["John", "Peter", "Keith"]
-	}
-},{
-	questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
-	choices: {
-		correct: "Phillip",
-		incorrect: ["John", "Peter", "Keith"]
-	}
+    questionString: "What is the name of Jen's date, who asks her along to see'Gay-A Gay Musical!'?",
+    choices: ["John", "Peter", "Keith", "Paul"]
+}, {
+    questionString: "Which topic does Moss end up discussing after accidentally ending up on the television news as opposed to Dragons Den?",
+    choices: ["Iraq War", "Afghanistan War", "European Union", "African Food Crisis"]
+}, {
+    questionString: "What football team does Roy unwittingly say he supports?",
+    choices: ["Chelsea", "Tottenham", "West Ham", "Arsenal"]
+}, {
+    questionString: "Which nine letter word does Moss clain is real when he appears on Countdown?",
+    choices: ["SNATTINBE", "TNETENNBA", "DREMYTABE", "RENNTOIRE"]
+}, {
+    questionString: "What is the nickname Moss is given when he joins a club after appearing on Countdown?",
+    choices: ["Prime", "Word", "Negative One", "8+"]
+
 }];
+
+// var allQuestions = [
+//   "this",
+//   "that",
+//   "thot",
+//   "trot"
+// ];
 // --------------------------------------------------------------------
 // Global variables
 // ----------------
@@ -49,8 +63,27 @@ var number = 60;
 // ----------------------
 
 // create for loop to generate divs to hold questions and answers
-for (i = 0; i < allQuestions.length; i++) {
-    $("#subContent").html("<div>" + allQuestions[i] + "</div>");
+function makesQuiz() {
+    // Looked into .each method but got to a roadblock.
+    // jQuery.each(allQuestions, function(questionString, choices) {
+    //   $("#questionsGoInHere").append(questionString + " " + choices);
+    // });
+    // Tried doing traditional loop but could not figure a way to create new div with each iterated question.
+    // for (i=0, i<allQuestions.length; i++) {
+    //   $("#questions").append("<div>" + allQuestions.questionString[i] + "</div>");
+    // }
+    // Trying out this example from online.
+    var html = "";
+    $.each(allQuestions, function(key, value) {
+        // html += "<h3>" + key + "</h3>";
+        $("#questions").html(value);
+        $.each(value, function(month, file) {
+            html += "<h1>" + month + "</h1>";
+            $.each(file, function(size, name) {
+                html += '<a href="' + name + '" data-role="button" data-inline="true" data-mini="true">' + size + ' </a>';
+            });
+        });
+    });
 }
 
 function run() {
@@ -80,10 +113,11 @@ function decrement() {
 
 function stop() {
 
-  console.log("stopping");
-  clearInterval(intervalId);
+    console.log("stopping");
+    clearInterval(intervalId);
+    $
 
 }
-    // --------------------------------------------------------------------
-    // Start game function
-    // -------------------
+// --------------------------------------------------------------------
+// Start game function
+// -------------------
